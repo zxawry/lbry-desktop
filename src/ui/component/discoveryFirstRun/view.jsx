289e4985d6047516153ab962175e126818c6c634
ポール ウェッブ <netopwibby@thenetwork.email>
2019-04-29 18:08:32 -0500

    Exploring visuals

--- src/ui/component/discoveryFirstRun/view.jsx
+++ src/ui/component/discoveryFirstRun/view.jsx
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
 
-const recommendedTags = ['crypto', 'science', 'idk', 'clicking me should do something'];
+const recommendedTags = [
+  'crypto',
+  'food',
+  'music',
+  'science',
+  'vlog',
+  'crypto',
+  'food',
+  'music',
+  'science',
+  'vlog',
+  'crypto',
+  'food',
+  'music',
+  'science',
+  'vlog',
+];
 
 export default function DiscoveryFirstRun() {
   const [userTags, setUserTags] = useState({});
   const clickTag = tag => {
     const newUserTags = { ...userTags };
+
     if (userTags[tag]) {
       delete newUserTags[tag];
     } else {
@@ -16,13 +33,22 @@ export default function DiscoveryFirstRun() {
   };
 
   return (
-    <div style={{ height: 400 }}>
-      <h2>Discover sum stuff</h2>
-      {recommendedTags.map(tag => (
-        <div style={userTags[tag] && { color: 'red' }} onClick={() => clickTag(tag)}>
-          {tag}
-        </div>
-      ))}
-    </div>
+    <section class="discover-section">
+      <header class="discover-section__header">
+        <h2 class="discover-section__title">Discover</h2>
+      </header>
+
+      <ul class="discover-section__tags">
+        {recommendedTags.map(tag => (
+          <li
+            class="discover-section__tag"
+            style={userTags[tag] && { color: 'red' }}
+            onClick={() => clickTag(tag)}
+          >
+            {tag}
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
