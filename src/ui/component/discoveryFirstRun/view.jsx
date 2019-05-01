ce9320d05238e88bddd4626eed2e484360695ecf
ポール ウェッブ <netopwibby@thenetwork.email>
2019-05-01 13:32:54 -0500

    Added an input field and did more design

--- src/ui/component/discoveryFirstRun/view.jsx
+++ src/ui/component/discoveryFirstRun/view.jsx
@@ -1,22 +1,75 @@
 import React, { useState } from 'react';
+import { TagCloud } from 'react-tagcloud';
 
 const recommendedTags = [
-  'crypto',
-  'food',
-  'music',
-  'science',
-  'vlog',
-  'crypto',
-  'food',
-  'music',
-  'science',
-  'vlog',
-  'crypto',
-  'food',
-  'music',
-  'science',
-  'vlog',
+  {
+    count: 25,
+    value: 'crypto',
+  },
+  {
+    count: 10,
+    value: 'food',
+  },
+  {
+    count: 7,
+    value: 'music',
+  },
+  {
+    count: 20,
+    value: 'science',
+  },
+  {
+    count: 16,
+    value: 'vlog',
+  },
+  {
+    count: 18,
+    value: 'bitcoin',
+  },
+  {
+    count: 4,
+    value: 'mazacoin',
+  },
+  {
+    count: 15,
+    value: 'namecoin',
+  },
+  {
+    count: 30,
+    value: 'neucoin',
+  },
+  {
+    count: 9,
+    value: 'nxt',
+  },
+  {
+    count: 15,
+    value: 'peercoin',
+  },
+  {
+    count: 12,
+    value: 'titcoin',
+  },
+  {
+    count: 1,
+    value: 'auroracoin',
+  },
 ];
+
+const customRenderer = (tag, size, color) => (
+  <span
+    className="discover-section__tag"
+    key={tag.value}
+    style={{
+      borderColor: color,
+      fontSize: `${size}rem`,
+      height: `calc(${size}rem * 4)`,
+      width: `calc(${size}rem * 4)`,
+    }}
+  >
+    {tag.value}
+  </span>
+);
 
 export default function DiscoveryFirstRun() {
   const [userTags, setUserTags] = useState({});
@@ -38,17 +91,16 @@ export default function DiscoveryFirstRun() {
         <h2 class="discover-section__title">Discover</h2>
       </header>
 
-      <ul class="discover-section__tags">
-        {recommendedTags.map(tag => (
-          <li
-            class="discover-section__tag"
-            style={userTags[tag] && { color: 'red' }}
-            onClick={() => clickTag(tag)}
-          >
-            {tag}
-          </li>
-        ))}
-      </ul>
+      <TagCloud
+        className="discover-section__tags"
+        maxSize={2}
+        minSize={1}
+        tags={recommendedTags}
+        onClick={tag => alert(`'${tag.value}' was selected!`)}
+        renderer={customRenderer}
+      />
+
+      <input class="discover-section__search" placeholder="Search for a specific tag" type="text" />
     </section>
   );
 }
