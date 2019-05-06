46bc64865d91d365536710d6136c6bef0de9418f
ポール ウェッブ <netopwibby@thenetwork.email>
2019-05-06 11:13:24 -0500

    Building blocks

--- src/ui/component/discoveryFirstRun/view.jsx
+++ src/ui/component/discoveryFirstRun/view.jsx
@@ -1,106 +1,62 @@
-import React, { useState } from 'react';
-import { TagCloud } from 'react-tagcloud';
+import React, { useEffect, useState } from 'react';
+import vis from 'vis';
 
-const recommendedTags = [
-  {
-    count: 25,
-    value: 'crypto',
-  },
-  {
-    count: 10,
-    value: 'food',
-  },
-  {
-    count: 7,
-    value: 'music',
-  },
-  {
-    count: 20,
-    value: 'science',
-  },
-  {
-    count: 16,
-    value: 'vlog',
-  },
-  {
-    count: 18,
-    value: 'bitcoin',
-  },
-  {
-    count: 4,
-    value: 'mazacoin',
-  },
-  {
-    count: 15,
-    value: 'namecoin',
-  },
-  {
-    count: 30,
-    value: 'neucoin',
-  },
-  {
-    count: 9,
-    value: 'nxt',
-  },
-  {
-    count: 15,
-    value: 'peercoin',
-  },
-  {
-    count: 12,
-    value: 'titcoin',
-  },
-  {
-    count: 1,
-    value: 'auroracoin',
-  },
-];
+const edges = new vis.DataSet();
 
-const customRenderer = (tag, size, color) => (
-  <span
-    className="discover-section__tag"
-    key={tag.value}
-    style={{
-      borderColor: color,
-      fontSize: `${size}rem`,
-      height: `calc(${size}rem * 4)`,
-      width: `calc(${size}rem * 4)`,
-    }}
-  >
-    {tag.value}
-  </span>
-);
+const nodes = new vis.DataSet([
+  { label: 'Pop' },
+  { label: 'Alternative' },
+  { label: 'Rock' },
+  { label: 'Jazz' },
+  { label: 'Hits' },
+  { label: 'Dance' },
+  { label: 'Metal' },
+  { label: 'Experimental' },
+  { label: 'Rap' },
+  { label: 'Electronic' },
+]);
+
+const data = {
+  edges,
+  nodes,
+};
+
+const options = {
+  nodes: {
+    borderWidth: 0,
+    shape: 'circle',
+    color: {
+      background: '#F92C55',
+      highlight: {
+        background: '#F92C55',
+        border: '#F92C55',
+      },
+    },
+    font: {
+      color: '#fff',
+    },
+  },
+  physics: {
+    stabilization: false,
+    minVelocity: 0.01,
+    solver: 'repulsion',
+    repulsion: {
+      nodeDistance: 40,
+    },
+  },
+};
+
+// const container = document.getElementById('bubbles');
+// const network = new vis.Network(container, data, options);
+
+const wrapperRef = React.createRef();
 
 export default function DiscoveryFirstRun() {
-  const [userTags, setUserTags] = useState({});
-  const clickTag = tag => {
-    const newUserTags = { ...userTags };
+  useEffect(function() {
+    let network = new vis.Network(wrapperRef, data, options);
+    // Cleanup on unmount
+    // network = ...
+  }, []);
 
-    if (userTags[tag]) {
-      delete newUserTags[tag];
-    } else {
-      newUserTags[tag] = true;
-    }
-
-    setUserTags(newUserTags);
-  };
-
-  return (
-    <section class="discover-section">
-      <header class="discover-section__header">
-        <h2 class="discover-section__title">Discover</h2>
-      </header>
-
-      <TagCloud
-        className="discover-section__tags"
-        maxSize={2}
-        minSize={1}
-        tags={recommendedTags}
-        onClick={tag => alert(`'${tag.value}' was selected!`)}
-        renderer={customRenderer}
-      />
-
-      <input class="discover-section__search" placeholder="Search for a specific tag" type="text" />
-    </section>
-  );
+  return <div ref={wrapperRef} />;
 }
